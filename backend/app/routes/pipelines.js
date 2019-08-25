c2c3116de53295105ab5cd5da1ef3379e6c4bd65
Mikhail Barabanov <barabanov.mikle@gmail.com>
2019-08-25 23:13:48 +0300

    you can now launch c# cli from api and get result

--- backend/app/routes/pipelines.js
+++ backend/app/routes/pipelines.js
@@ -1,8 +1,30 @@
 import { Router } from 'express';
-const router = Router();
+import Docker from 'dockerode';
+import Streams from 'memory-streams';
 
+const router = Router();
 router.get('/', (req, res) => {
   return res.send('Pipelines');
+});
+
+router.get('/run', async (req, res) => {
+  var docker = new Docker({socketPath: process.env.DOCKER_SOCKET});
+  
+  try {    
+    // todo: need handling of missing container, now just add container to docker-compose
+    var writer = new Streams.WritableStream(); 
+    var container = await docker.run(process.env.CONTAINER_IMAGE_TO_RUN, [], writer);
+    
+    await container.wait();
+    var result = writer.toString();
+    await container.remove();
+
+    
+    return res.send('Pipeline finished with result:' + result);
+  } catch(err) {
+    return res.send(err);
+  }
+
 });
 
 export default router;
