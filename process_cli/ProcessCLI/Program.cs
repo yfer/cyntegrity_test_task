715fcaeb4c96acb6b47abb0a6cdf4ed1cfd907a5
Mikhail Barabanov <barabanov.mikle@gmail.com>
2019-08-25 17:22:12 +0300

    Simple c#.net.core console app and Dockerfile for it

---
+++ process_cli/ProcessCLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProcessCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
